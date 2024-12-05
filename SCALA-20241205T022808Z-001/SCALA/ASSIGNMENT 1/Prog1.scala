c5d3bb5ce9adb3d4bce07f2b4cb7d862f710ffca
Sanketb04 <149719945+Sanketb04@users.noreply.github.com>
2024-12-05 20:22:53 +0530

    Delete SCALA-20241205T022808Z-001 directory

--- SCALA-20241205T022808Z-001/SCALA/ASSIGNMENT 1/Prog1.scala
+++
@@ -1,9 +0,0 @@
-object Prog1{
-def main(args:Array[String]):Unit={
-print("Enter the number")
-var a= scala.io.StdIn.readDouble()
-if(a>0){print(s"$a is Positive")}
-else if(a<0){print(s"$a is Negative")}
-else {print(s"$a is zerooo")}
-}
-}
